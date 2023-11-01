01948ef8523520507d13ba90c79378d78801f03a
clara <clasei@proton.me>
2023-11-01 22:10:32 +0100

    errors solved, localhost running

--- src/components/ProjectLinks/ProjectLinks.js
+++ src/components/ProjectLinks/ProjectLinks.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './ProjectLinks.css';
-import projectLogo from '../../logos/lego-brick.svg';
+import projectLogo from '../../assets/logos/lego-brick.svg';
 
 const ProjectLinks = ({ project }) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,7 +13,7 @@ const ProjectLinks = ({ project }) => {
         <div className="project-card" onClick={toggleOpen}>
             <div className="project-header">
                 <img src={projectLogo} alt="Project logo" className="project-logo"/>
-                <h2>{project.title}</h2>
+                <h2>twinkleCircle</h2>
             </div>
             {isOpen && (
                 <div className="project-details">
