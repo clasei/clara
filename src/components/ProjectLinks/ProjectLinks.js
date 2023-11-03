7e30e93b1b3169ee33aff47c1c2e5ee10fef988f
clara <clasei@proton.me>
2023-11-03 16:34:21 +0100

    restructure started

--- src/components/ProjectLinks/ProjectLinks.js
+++ src/components/ProjectLinks/ProjectLinks.js
@@ -3,26 +3,15 @@ import './ProjectLinks.css';
 import projectLogo from '../../assets/logos/lego-brick.svg';
 
 const ProjectLinks = ({ project }) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleOpen = () => {
-        setIsOpen(!isOpen);
-    }
-
     return (
-        <div className="project-card" onClick={toggleOpen}>
+        <a href="https://clasei.github.io/twinkle-circle/" target="_blank" rel="noopener noreferrer" className="project-card">
             <div className="project-header">
-                <img src={projectLogo} alt="Project logo" className="project-logo"/>
+                <img src={projectLogo} alt="Project logo" className="project-logo" />
                 <h3>twinkleCircle</h3>
             </div>
-            {isOpen && (
-                <div className="project-details">
-                    <p>{project.description}</p>
-                    <a href={project.link} target="_blank" rel="noopener noreferrer">Ver proyecto</a>
-                </div>
-            )}
-        </div>
-    )
+        </a>
+    );
 }
 
 export default ProjectLinks;
+
